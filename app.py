adaf2ffa19ddbefcc60917865e846b5092d2d45d
Aronja <anovicky@thoughtworks.com>
2019-08-03 23:25:58 +0100

    Simple Post mapping

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, Response, request, abort
+import json
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/kotlin', methods=['POST'])
+def parse_code():
+    if request.method == 'POST':
+        if not request.json:
+            abort(400)
+        print(request.json)    
+    return Response(json.dumps(request.json))
 
 if __name__ == '__main__':
     app.run()
