5d661a091c42bfa1244ab068d2aa0e399aa0f56f
Aronja <anovicky@thoughtworks.com>
2019-08-08 20:51:31 +0100

    Trying to understand how jar files are created

--- kotlin.kt
+++ kotlin.kt
@@ -1,1 +1,1 @@
-fun main() { println("hello kotlin!")}
+fun main(args: Array<String>) {println("Hello, World!")}
--- kotlin_compiler.sh
+++ kotlin_compiler.sh
@@ -1,3 +1,5 @@
 echo kotlin will be compiled
 kotlinc kotlin.kt -include-runtime -d kotlin.jar
+echo kotlin is compiled
+java -jar kotlin.jar
 echo done!
